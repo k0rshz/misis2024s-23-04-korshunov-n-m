ee4550b1512afd75c87cb8b51f8635cf96ec54b0
Никита Коршунов <m2303488@edu.misis.ru>
2024-02-12 21:20:15 +0300

    upd

--- prj.lab/stackarr/stackarr.cpp
+++ prj.lab/stackarr/stackarr.cpp
@@ -7,35 +7,55 @@ public:
 	~StackArr() {
 		delete[] data_;
 	}
-	StackArr(const StackArr&);
-	StackArr& operator=(const StackArr&);
-	void Pop();
-	void Push(Complex a);
-	bool IsEmpty();
-	Complex Top();
+	StackArr(const StackArr& rhs);
+	StackArr& operator=(const StackArr& rhs);
+	void pop();
+	void push(const Complex& a);
+	bool isEmpty() const;
+	Complex& top();
 private:
 	Complex* data_ = nullptr;
 	int capacity_ = 0;
 	int topInd_ = -1;
 };
 
-void StackArr::Push(Complex a) {
+StackArr::StackArr(const StackArr& rhs) {
+	capacity_ = rhs.capacity_;
+	topInd_ = rhs.topInd_;
+	data_ = new Complex[capacity_];
+	for (int i = 0; i <= topInd_; ++i) {
+		data_[i] = rhs.data_[i];
+	}
+}
+
+StackArr& StackArr::operator=(const StackArr& rhs) {
+	delete[] data_;
+	capacity_ = rhs.capacity_;
+	topInd_ = rhs.topInd_;
+	data_ = new Complex[capacity_];
+	for (int i = 0; i <= topInd_; ++i) {
+		data_[i] = rhs.data_[i];
+	}
+	return *this;
+}
+
+void StackArr::push(const Complex& a) {
 	if (topInd_ == capacity_ - 1) {
-		int ncapacity = capacity_ + 1;
+		int ncapacity = (capacity_ + 1) * 2;
 		Complex* ndata = new Complex[ncapacity];
-		topInd_ += 1;
-		for (int i = 0; i < topInd_; ++i) {
+		for (int i = 0; i <= topInd_; ++i) {
 			ndata[i] = data_[i];
 		}
 		delete[] data_;
 		capacity_ = ncapacity;
 		data_ = ndata;
 	}
+	topInd_ += 1;
 	data_[topInd_] = a;
 }
 
-void StackArr::Pop() {
-	Complex* ndata = new Complex[capacity_ - 1];
+void StackArr::pop() {
+	Complex* ndata = new Complex[capacity_];
 	for (int i = 0; i < topInd_; ++i) {
 		ndata[i] = data_[i];
 	}
@@ -44,25 +64,25 @@ void StackArr::Pop() {
 	data_ = ndata;
 }
 
-Complex StackArr::Top() {
+Complex& StackArr::top() {
 	return data_[topInd_];
 }
 
-bool StackArr::IsEmpty() {
+bool StackArr::isEmpty() const {
 	return topInd_ == -1;
 }
 
 int main() {
 	Complex a{ 1.5, 2.0 }, b{ 3.1, 4.9 }, c{ 2.8, 3.1 };
 	StackArr s;
-	std::cout << s.IsEmpty() << std::endl;
-	s.Push(a);
-	std::cout << s.IsEmpty() << std::endl;
-	std::cout << s.Top() << std::endl;
-	s.Push(b);
-	std::cout << s.Top() << std::endl;
-	s.Pop();
-	std::cout << s.Top() << std::endl;
-	s.Push(c);
-	std::cout << s.Top() << std::endl;
+	std::cout << s.isEmpty() << std::endl;
+	s.push(a);
+	std::cout << s.isEmpty() << std::endl;
+	std::cout << s.top() << std::endl;
+	s.push(b);
+	std::cout << s.top() << std::endl;
+	s.pop();
+	std::cout << s.top() << std::endl;
+	s.push(c);
+	std::cout << s.top() << std::endl;
 }
